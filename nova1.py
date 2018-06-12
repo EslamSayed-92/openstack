bfb83f6aeff1110447ff395552598fb82437befe
EslamSayed92 <eslam.92.sayed@gmail.com>
2018-06-12 16:43:02 +0200

    created network using neutronclient

--- nova1.py
+++ nova1.py
@@ -9,50 +9,56 @@ def show_attr(obj):
 	for item in dir(obj):
 		print(item, type(getattr(obj,item)))
 
+
 VERSION = "2.1"
+DOMAIN = 'default'
 USERNAME = 'admin'
-PASSWORD = 'redhat'
-AUTH_URL = 'http://10.70.71.1:5000/v2.0'
-PROJECT_ID = '85b14409730241139975ff4d9f44bbc6'
+PASSWORD = 'Moustafa11'
+AUTH_URL = 'http://192.168.20.131/identity'
+PROJECT_ID = '730ac87b1e03449c8546f22440b17ff6'
 
 loader = loading.get_plugin_loader('password')
-auth = loader.load_from_options(auth_url=AUTH_URL,username=USERNAME,password=PASSWORD,project_id=PROJECT_ID)
+auth = loader.load_from_options(auth_url=AUTH_URL,user_domain_name=DOMAIN,username=USERNAME,password=PASSWORD,project_id=PROJECT_ID)
 sess = session.Session(auth=auth)
 
-nova = client.Client(VERSION, session=sess)
-glance = glclient.Client(session=sess)
-neutron = neuclient.Client(session=sess)
 
-network = {'name': 'mynetwork', 'admin_state_up': True}
-neutron.create_network({'network':network})
+#---------- Compute Client -----------------------#
+nova = client.Client('2.1', session=sess)
 
-image = list(glance.images.list())[0]
-#print(image.id)
-
-#nova = client.Client(VERSION, USERNAME, PASSWORD, PROJECT_ID,AUTH_URL)
-instances = nova.servers.list()
-
-#for item in dir(nova):
-#	print(item, type(getattr(nova,item)))
+#show_attr(nova)
 #print("------------------------------------")
-#for item in dir(nova.glance):
-#	print(item, type(getattr(nova.glance,item)))
+#print(nova.glance.list())
 #print("------------------------------------")
-#for item in dir(nova.neutron):
-#	print(item, type(getattr(nova.neutron,item)))
-show_attr(neutron.list())
+#show_attr(nova.neutron.list())
+#print("------------------------------------")
+#print(nova.servers.list())
+#print("------------------------------------")
 
 
+# ---------- Creating Nova Instance -------------#
 #print("Network:\r\n")
 #print(nova.neutron.find_network("privete2").id)
-netid = nova.neutron.find_network("privete2").id
+#netid = nova.neutron.find_network("privete2").id
 #print("--------------------------------------\r\n")
-#print("Images:\r\n")
-#print(dir(nova.glance))
+
+#print("Image:\r\n")
+#print(nova.glance.list())
+#image = nova.glance.list()[0]
 #print("-------------------------------------------")
 
-
-#print(nova.servers.list())
-
-fl = nova.flavors.find(ram=512)
+#fl = nova.flavors.find(ram=512)
 #print(nova.servers.create("apiTest", flavor=fl,image=image,nics=[{'net-id':netid}]))
+
+
+#--------------- Image Client ----------------------#
+glance = glclient.Client(session=sess)
+
+
+#---------------- Network Client -------------------#
+neutron = neuclient.Client(session=sess)
+
+#network = {'name': 'mynetwork', 'admin_state_up': True}
+#print(neutron.create_network({'network':network}))
+#show_attr(neutron)
+print("-------------------------")
+print(neutron.show_network("103e8e54-8846-44b6-a9bd-8736facdf6f2"))
